296a76edfc53e19cb2cc19f5618814b484781a19
WalterAhza <110404746+NotAhza555@users.noreply.github.com>
2023-07-21 19:30:11 +0700

    SiPintar.py

--- SiPintar.py
+++ SiPintar.py
@@ -1,28 +1,43 @@
 import discord
-from test import Generator 
+from discord.ext import commands
+import random
 
 # Variabel intents menyimpan hak istimewa bot
 intents = discord.Intents.default()
 # Mengaktifkan hak istimewa message-reading
 intents.message_content = True
 # Membuat bot di variabel klien dan mentransfernya hak istimewa
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='/', intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'Kita telah masuk sebagai {client.user}')
+    print(f'Kita telah masuk sebagai {bot.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
+@bot.command()
+async def hello(ctx):
+     await ctx.send(f'Hi! I am a bot {bot.user}!')
+
+@bot.command()
+async def hah(ctx, count_hah = 5):
+    await ctx.send("ha" * count_hah)
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} Memasuki server {discord.utils.format_dt(member.joined_at)}')
+
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
         return
-    if message.content.startswith('/halo'):
-        await message.channel.send("Hi!")
-    elif message.content.startswith('/bye'):
-        await message.channel.send("👋")
-    elif message.content.startswith('/generate'):
-        await message.channel.send(Generator(10))
-    else:
-        await message.channel.send(message.content)
 
-client.run("")
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
+    
+
+
+bot.run()
